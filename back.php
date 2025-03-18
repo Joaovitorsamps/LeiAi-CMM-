f6268c2082191c713771898846668a03434f6d4b
82988129215 <joaovitorsamps@hotmail.com>
2025-03-18 12:04:31 -0400

    Primeiro Commit

---
+++ back.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LeiAí</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav class="cabecalho">
+            <div class="cabecalho__maior">
+                    <img class="cabecalho__logo" src="./imgs/logo.png" alt="logo do projeto">
+                    <div>
+                        <h1 class="cabecalho__titulo">Portal LeiAí</h1>
+                        <h2 class="cabecalho__subtitulo">PORTAL DA INTERAÇÃO DA POPULAÇÃO DO AMAZONAS</h2>
+                    </div>
+                    
+                <div class="cabecalho__login">
+                    <input class="cabecalho__login__caixa" type="text">
+                    <a class="cabecalho__login__link" href="login.html">Entrar/Cadastrar</a>
+                </div>
+            </div>
+            <div class="cabecalho__menor">
+                    <a class="cabecalho__menor__links" href="about.html">Sobre o Portal</a>
+                    <a class="cabecalho__menor__links" href="index.html">Início</a>
+                    <a class="cabecalho__menor__links" href="lawsprojects.html">Propostas de Leis</a>
+                    <a class="cabecalho__menor__links" href="aboutthelaws.html">Sobre o Legislativo</a>
+            </div>
+        </nav>
+    </header>
+    <main class="corpo">
+        <h2 class="corpo__titulo">PROPOSTAS EM TRAMITE NO LEGISLATIVO</h2>
+        <div class="corpo__container">
+            <div class="corpo__container__titulos">
+                <h3>Veto a Projeto de Lei nº 00 de 20XX</h3><br>
+                <h4>Autoria: Poder Executivo Parlamentar</h4><br>
+                <h4>Tipo do Projeto: Veto a Projeto de Lei</h4><br>
+                <h4>Ementa: VETO PARCIAL, incidente sobre o artigo X.º, do Projeto de Lei n. 0000/000, de autoria dos veradores Ana e Lucas.</h4>
+                <br>
+                <div class="corpo__container__titulos__botoes">
+                    <a class="corpo__container__titulos__linksaceito" href="#">EU APOIO</a>
+                   <a class="corpo__container__titulos__linksnao" href="#">EU NÃO APOIO</a>
+                </div>
+            </div>
+            <img class="corpo__container__imagem" src="imgs/imagem_vereador.jpeg" alt=""> 
+        </div>
+        <div class="corpo__container">
+            <div class="corpo__container__titulos">
+                <h3>Veto a Projeto de Lei nº 00 de 20XX</h3><br>
+                <h4>Autoria: Poder Executivo Parlamentar</h4><br>
+                <h4>Tipo do Projeto: Veto a Projeto de Lei</h4><br>
+                <h4>Ementa: VETO PARCIAL, incidente sobre o artigo X.º, do Projeto de Lei n. 0000/000, de autoria dos veradores Ana e Lucas.</h4>
+                <br>
+                <div class="corpo__container__titulos__botoes">
+                    <a class="corpo__container__titulos__linksaceito" href="#">EU APOIO</a>
+                   <a class="corpo__container__titulos__linksnao" href="#">EU NÃO APOIO</a>
+                </div>
+            </div>
+            <img class="corpo__container__imagem" src="imgs/imagem_vereador.jpeg" alt=""> 
+        </div>    
+    </main>
+    <footer class="rodape">
+        <p>Projeto realizado pela turma de Sistemas de Informação do 2º Período, da faculdade FAMETRO Manaus.</p>
+    </footer>
+
+</body></html>
